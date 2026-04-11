54061c196380a8ae112294c08d688a58d825914c
JadenPlummx <jaden.plummer@dcmail.ca>
2026-04-11 18:44:54 -0400

    Fixed Ball constructor for Shape inheritance

--- lab4/part4/script.js
+++ lab4/part4/script.js
@@ -34,6 +34,10 @@ class Shape {
 class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
 
   draw() {
     ctx.beginPath();
